22af9edda0f2da848fe7fc8265d5f69c8b77936a
Devin0131 <960094569@qq.com>
2023-05-16 20:45:33 +0800

    第一次提交

---
+++ init.lua
@@ -0,0 +1,40 @@
+return {
+	plugins = {
+		{
+			"phaazon/hop.nvim",
+			config = true;
+			keys = {
+				{'f', function() 
+					local directions = require('hop.hint').HintDirection
+					require('hop').hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true }) end},
+				{'F', function() 
+					local directions = require('hop.hint').HintDirection
+					require('hop').hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true }) end},
+				{'t', function() 
+					local directions = require('hop.hint').HintDirection
+					require('hop').hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }) end},
+				{'T', function() 
+					local directions = require('hop.hint').HintDirection
+					require('hop').hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }) end}
+			}
+
+		}
+		,{
+			"lervag/vimtex",
+			config = function() 
+				vim.cmd('filetype plugin indent on')
+
+				-- syntax enable
+
+				-- vim.g.vimtex_view_method = 'zathura'
+				vim.g.vimtex_view_general_viewer = '/home/devin/.local/bin/sumatrapdf.sh'
+				vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
+				-- vim.g.vimtex_compiler_method = 'latexrun'
+
+				vim.cmd('let maplocalleader = ";"')
+			end,
+			ft = {"tex"}
+		},
+	}
+}
+
