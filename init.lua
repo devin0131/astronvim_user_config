28c60831e591edd82b2d891cdde24a62d7c5fbb9
devin0131 <960094569@qq.com>
2023-05-19 20:32:36 +0800

    修复了vimtex的bug

--- init.lua
+++ init.lua
@@ -2,39 +2,41 @@ return {
 	plugins = {
 		{
 			"phaazon/hop.nvim",
-			config = true;
+			config = true,
 			keys = {
-				{'f', function() 
+				{ 'f', function()
 					local directions = require('hop.hint').HintDirection
-					require('hop').hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true }) end},
-				{'F', function() 
+					require('hop').hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+				end },
+				{ 'F', function()
 					local directions = require('hop.hint').HintDirection
-					require('hop').hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true }) end},
-				{'t', function() 
+					require('hop').hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
+				end },
+				{ 't', function()
 					local directions = require('hop.hint').HintDirection
-					require('hop').hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }) end},
-				{'T', function() 
+					require('hop').hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
+				end },
+				{ 'T', function()
 					local directions = require('hop.hint').HintDirection
-					require('hop').hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }) end}
+					require('hop').hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
+				end }
 			}
-
 		}
-		,{
-			"lervag/vimtex",
-			config = function() 
-				vim.cmd('filetype plugin indent on')
+		, {
+		"lervag/vimtex",
+		config = function()
+			vim.cmd('filetype plugin indent on')
 
-				-- syntax enable
+			-- syntax enable
 
-				-- vim.g.vimtex_view_method = 'zathura'
-				vim.g.vimtex_view_general_viewer = '/home/devin/.local/bin/sumatrapdf.sh'
-				vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
-				-- vim.g.vimtex_compiler_method = 'latexrun'
+			vim.g.vimtex_view_method = 'zathura'
+			-- vim.g.vimtex_view_general_viewer = '/home/devin/.local/bin/sumatrapdf.sh'
+			-- vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
+			-- vim.g.vimtex_compiler_method = 'latexrun'
 
-				vim.cmd('let maplocalleader = ";"')
-			end,
-			ft = {"tex"}
-		},
+			vim.cmd('let maplocalleader = ";"')
+		end,
+		lazy = false
+	},
 	}
 }
-
