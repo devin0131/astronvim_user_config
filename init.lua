58e2569078d2985163b56b8376dfa1372c49df16
devin0131 <960094569@qq.com>
2023-07-25 13:44:31 +0800

    将toggleterm的触发方式调整到horizontal

--- init.lua
+++ init.lua
@@ -1,13 +1,30 @@
 return {
+	-- colorscheme = "melange",
+	-- colorscheme = "github_dark",
+	-- mappings = {
+	-- 	n = {
+	-- 		["j"] = nil,
+	-- 		["k"] = nil
+	-- 	}
+	-- },
+	colorscheme = "github_light",
+	-- colorscheme = "melange",
 	options = {
 		opt = {
-			shell = "/usr/bin/bash"
+			shell = "/bin/bash",
+			signcolumn = "no",
+			foldcolumn = "0",
+			number = false,
+			relativenumber = true,
+			laststatus = 0,
+			showtabline = 0,
+			-- number = true,
+			-- relative_number = false,
 		},
 	},
 	lsp = {
 		servers = {
 			"clangd",
-			"lemminx",
 		},
 		config = {
 			clangd = {
@@ -15,15 +32,28 @@ return {
 				cmd = { 'clangd', '--background-index', '-j=8', '--clang-tidy',
 					'--all-scopes-completion', '--completion-style=detailed' }
 			},
-			lemminx = {
-				cmd = { '/home/devin/lemminx/org.eclipse.lemminx/target/lemminx-linux-aarch_64-0.26.1-SNAPSHOT' }
-			}
 		}
 	},
 	plugins = {
 		{
 			"AstroNvim/astrocommunity",
 			{ import = "astrocommunity.utility.noice-nvim" },
+			{ import = "astrocommunity.colorscheme.gruvbox-nvim" },
+			{ import = "astrocommunity.colorscheme.github-nvim-theme" },
+			{ import = "astrocommunity.colorscheme.melange-nvim" },
+			{ import = "astrocommunity.colorscheme.nightfox-nvim" },
+			{ import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+			{ import = "astrocommunity.scrolling.mini-animate" },
+			-- {
+			-- 	"echasnovski/mini.animate",
+			-- 	opts = function (_, opts)
+			-- 		local animate = require("mini.animate")
+			-- 		opts.cursor = {
+   --      		timing = animate.gen_timing.linear { duration = 80, unit = "total" },
+   --    		}
+			-- 		return opts
+			-- 	end
+			-- },
 			{ import = "astrocommunity.motion.hop-nvim" },
 			{
 				"phaazon/hop.nvim",
@@ -67,35 +97,40 @@ return {
 			-- { import = "astrocommunity.motion.flash-nvim" },
 			{ import = "astrocommunity.pack.java" },
 			-- 下边的两个配置是因为mason可能不会安装指定的插件，所以我们不要它装我们自己装
-			{
-				"williamboman/mason-lspconfig.nvim",
-				opts = function(_, opts)
-					opts.ensure_installed['lemminx'] = nil
-					for k, v in pairs(opts.ensure_installed) do
-						if v == 'lemminx' then
-							table.remove(opts.ensure_installed, k)
-						end
-					end
-				end,
-			},
-			{
-				"jose-elias-alvarez/null-ls.nvim",
-				-- opts = {
-				-- 	sources = {
-				-- 		require("null-ls").builtins.formatting.clang_format,
-				-- 	}
-				-- }
-				config = function(_, opts)
-					local null_ls = require("null-ls")
-					local source = { null_ls.builtins.formatting.clang_format }
-					null_ls.register(source)
-					null_ls.setup(opts)
-				end
-			},
-			{
-				"jay-babu/mason-null-ls.nvim",
-				opts = function(_, opts) opts.ensure_installed = {} end,
-			},
+		},
+		{
+			"rainbowhxch/accelerated-jk.nvim",
+			-- lazy = false,
+			-- keys = function (self,keys)
+			-- 	-- local utils = require "astronvim.utils"
+			-- 	-- local maps = require("astronvim.utils").empty_map_table()
+			-- 	-- maps = astronvim.user_opts("mappings")
+			-- 	-- print(maps)
+			-- 	-- print(maps.n)
+			-- 	-- maps.n["j"] = nil
+			-- 	-- maps.n["k"] = nil
+			-- 	-- utils.set_mappings(maps)
+			-- 	keys = {
+			-- 		{"j", "<Plug>(accelerated_jk_gj)"},
+			-- 		{"k", "<Plug>(accelerated_jk_gk)"}
+			-- 	}
+			-- 	return keys
+			-- end,
+			keys = {"j", "k"},
+			opts = function(_, opts)
+				vim.keymap.set("n", "j", "<Plug>(accelerated_jk_gj)")
+				vim.keymap.set("n", "k", "<Plug>(accelerated_jk_gk)")
+				return {
+    		mode = 'time_driven',
+    		enable_deceleration = false,
+    		acceleration_motions = {},
+    		acceleration_limit = 150,
+    		-- acceleration_table = { 7,12,17,21,24,26,28,30 },
+    		acceleration_table = {5,10, 15},
+    		-- when 'enable_deceleration = true', 'deceleration_table = { {200, 3}, {300, 7}, {450, 11}, {600, 15}, {750, 21}, {900, 9999} }'
+    		deceleration_table = { {150, 9999} }
+			}
+			end
 		},
 		{
 			"lervag/vimtex",
@@ -119,23 +154,24 @@ return {
 			"iamcco/markdown-preview.nvim",
 			cmd = { "MarkdownPreviewToggle" },
 			build = "cd app && yarn install",
-			init = function()
-				vim.g.mkdp_filetypes = { "markdown" }
-				vim.g.mkdp_open_ip = '0.0.0.0'
-				vim.g.mkdp_open_to_the_world = 1
-				vim.cmd([[
-				function! g:EchoUrl(url)
-				:echo a:url
-				endfunction
-				]])
-				vim.g.mkdp_browserfunc = 'g:EchoUrl'
-			end,
+			-- init = function()
+			-- 	vim.g.mkdp_filetypes = { "markdown" }
+			-- 	vim.g.mkdp_open_ip = '0.0.0.0'
+			-- 	vim.g.mkdp_open_to_the_world = 1
+			-- 	vim.cmd([[
+			-- 	function! g:EchoUrl(url)
+			-- 	:echo a:url
+			-- 	endfunction
+			-- 	]])
+			-- 	vim.g.mkdp_browserfunc = 'g:EchoUrl'
+			-- end,
 			ft = { "markdown" },
 		},
 		{
 			"akinsho/toggleterm.nvim",
 			opts = {
-				shell = "/usr/bin/fish"
+				shell = "/opt/homebrew/bin/fish",
+				direction = "horizontal"
 			}
 		},
 		{
